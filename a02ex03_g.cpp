4fa8fabab02023536659b7f8a7414b98a233fee4
brunorcorrea <correaricardobruno@gmail.com>
2023-05-30 18:50:19 -0300

    Test creating "CottageCheese" class

---
+++ a02ex03_g.cpp
@@ -0,0 +1,26 @@
+
+/* Unicamp - Universidade Estadual de Campinas
+   FT - Faculdade de Tecnologia
+   Limeira - SP
+   Prof. Dr. Andre F. de Angelis
+   Maio/2015
+*/
+
+#include <string>
+#include "a02ex03_f.hpp"
+#include "a02ex03_g.hpp"
+
+using namespace std;
+
+CottageCheese::CottageCheese(string tipo, float peso, double valor) : Cheese(tipo, peso, valor)
+   {
+   this->tipo = tipo;
+   this->peso = peso;
+   };
+   
+string CottageCheese::getDescricao()
+   {
+   return ("Queijo cottage" + tipo + " - " + to_string(peso) + " Kg.");
+   };
+   
+/* fim de arquivo */
