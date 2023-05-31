8e0efb9c99bc85df8b0ffe53a291edfd8334e265
brunorcorrea <correaricardobruno@gmail.com>
2023-05-31 18:52:13 -0300

    Test creating "Ham" class

---
+++ a02ex03_h.cpp
@@ -0,0 +1,25 @@
+/* Unicamp - Universidade Estadual de Campinas
+   FT - Faculdade de Tecnologia
+   Limeira - SP
+   Prof. Dr. Andre F. de Angelis
+   Maio/2015
+*/
+
+#include <string>
+#include "a02ex03_b.hpp"
+#include "a02ex03_h.hpp"
+
+using namespace std;
+
+Ham::Ham(string tipo, float peso, double valor) : Food(valor)
+   {
+   this->tipo = tipo;
+   this->peso = peso;
+   };
+   
+string Ham::getDescricao()
+   { 
+   return ("Presunto " + tipo + " - " + to_string(peso) + " Kg.");
+   };
+   
+/* fim de arquivo */
