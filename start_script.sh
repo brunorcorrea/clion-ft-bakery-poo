3453798741f8af379c89b4f84b42b2f83dab939e
brunorcorrea <correaricardobruno@gmail.com>
2023-06-06 10:35:51 -0300

    Creating a new script to run the project

---
+++ start_script.sh
@@ -0,0 +1,3 @@
+clear;
+g++ -std=c++11 Information.cpp MyBoolean.cpp Menu.cpp Food.cpp Bread.cpp Cracker.cpp FilledWafer.cpp Cheese.cpp CottageCheese.cpp Ham.cpp Mortadella.cpp MyProgram.cpp FT_bakery.cpp Liquid.cpp Milk.cpp Water.cpp Soda.cpp Beer.cpp PilsenBeer.cpp BockBeer.cpp PaleAleBeer.cpp LagerBeer.cpp -o ft_bakery.exe;
+./ft_bakery.exe
